944791008406e1067a259eaedc2a47b1616061fc
Raffael Di Pietro <rdipietro@formation-cfai.fr>
2022-10-30 18:34:44 +0100

    begin to implement strategy classs

---
+++ TP3/src/gameStrategy.hpp
@@ -0,0 +1,11 @@
+#ifndef GAME_STRATEGY
+#define GAME_STRATEGY
+
+#include <string>
+
+class GameStrategy {
+    public:
+        virtual ~GameStrategy() = default;
+        virtual void placeToken() const = 0;
+};
+#endif // GAME_STRATEGY
